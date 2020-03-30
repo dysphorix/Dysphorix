9189be9670df370c433d3f4c639054aca26de8e8
dysphorix <59848541+dysphorix@users.noreply.github.com>
2020-03-30 17:28:39 -0600

    Add files via upload

---
+++ CodingChallenge.java
@@ -0,0 +1,17 @@
+public class CodingChallenge
+{
+      //Gerardo Steve Lopez
+      
+   public static void main (String[]args)
+   {
+   double a =252.12;
+   double b =310.14;
+   double c = a * b;
+   System.out.println("*******************");   //Part One
+   System.out.println("*\t\t\t*\t\t\t*");
+   System.out.println("*\t\t\t*\t\t\t*");
+   System.out.println("*******************\n");
+   System.out.printf("%.2f times %.2f equals %,.4f.\n\n", a, b, c);   //Part Two
+   System.out.printf("%,.4f negated is %,.4f.\n", c, -c);   //Part Three
+   }
+}
